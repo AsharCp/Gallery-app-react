c2b21591ed1f577a56252417f733fd502a08145d
Your Name <you@example.com>
2023-07-17 14:15:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import Gallery from './components/Gallery'
+
+function App() {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
